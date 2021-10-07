d2e9c55ede2ef4729ff18bccccc2221f8145c0fa
Kri-hika <73770973+Kri-hika@users.noreply.github.com>
2021-10-08 03:20:54 +0530

    sawo implementation midway

--- Come_unity/app/models.py
+++ Come_unity/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+# import os
 
-# Create your models here.
+class Config(models.Model):
+  
+    api_key = models.CharField(max_length=200)
+    identifier = models.CharField(max_length=200,choices = [("email","Email"),("phone_number_sms","Phone")])
+
+
+
+
