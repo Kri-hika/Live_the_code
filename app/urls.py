27503086caa450cda5d61d3078b65a3aa9786ac4
utkarshamokashi <65499431+utkarshamokashi@users.noreply.github.com>
2021-10-09 10:49:03 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    # path("", views.home, name="home"),
-    path('', views.index),
-    # path("signin", views.signin, name="signin"),
-    path("signup", views.signup, name="signup"),
-    
-    path('signin', views.signin, name="signin"),
-    path('receive', views.receive)
+    path("signin", views.signin, name="signin"),
+    #path("signup", views.signup, name="signup"),
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("form", views.form, name="form"),
+    path("community", views.community, name="community"),
+    path("info", views.info, name="info"),
 ]
