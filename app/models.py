49463ab4dd455cc54a8c4a159a5f4066677526e1
utkarshamokashi <65499431+utkarshamokashi@users.noreply.github.com>
2021-10-09 10:49:35 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -8,4 +8,17 @@ class Config(models.Model):
 
 
 
+class Info(models.Model):
+    name = models.CharField( max_length=30)  # Field name made lowercase.
+    email = models.EmailField(max_length=254) # field name for email in lowercase
 
+
+
+class Customized_Info(models.Model):
+    c_name = models.CharField( max_length=30)  # Field name made lowercase.m
+    logo = models.ImageField(upload_to = 'pics') # field name for email in lowercase
+    bg_img =  models.ImageField(upload_to = 'pics')  # Field name made lowercase.m
+    url = models.URLField(max_length=231)# field name for email in lowercase
+    c_url = models.URLField(max_length=231) # Field name made lowercase.models
+    def __str__(self):
+        return self.name
