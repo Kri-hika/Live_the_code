9982f858ee7d60c3f4560bf031a490ad33507924
Kri-hika <73770973+Kri-hika@users.noreply.github.com>
2021-10-09 18:02:53 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,12 @@ from app import views
 
 urlpatterns = [
     path("signin", views.signin, name="signin"),
-    #path("signup", views.signup, name="signup"),
+    path("info", views.info, name="info"),
     path("dashboard", views.dashboard, name="dashboard"),
     path("form", views.form, name="form"),
     path("community", views.community, name="community"),
-    path("info", views.info, name="info"),
+    path("update_id", views.update_id, name="update_id"),
+    path("aboutUs", views.aboutUs, name="aboutUs"),
+    
 ]
+
